949d8386a39b5e8ae2cb5ea99387525b76ea0c2c
Adam Nichols <aednichols@gmail.com>
2020-12-02 17:40:05 -0500

    Add support for NPS banners

--- zen.js
+++ zen.js
@@ -1,5 +1,13 @@
 // https://stackoverflow.com/a/44089923/818054
+
+// Banners
 document.body.addEventListener('DOMSubtreeModified', function(event) {
-	const elements = document.getElementsByClassName("react-notification-root");
-	while (elements.length > 0) elements[0].remove();
+    const elements = document.getElementsByClassName("react-notification-root");
+    while (elements.length > 0) elements[0].remove();
+});
+
+// NPS score
+document.body.addEventListener('DOMSubtreeModified', function(event) {
+    const elements = document.getElementsByClassName("animate__animated animate__slideInRight");
+    while (elements.length > 0) elements[0].remove();
 });
